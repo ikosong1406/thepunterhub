2cc686102b3d08c7cc821198317ccbf866b4c281
ikosong14 <alexandervirtuous14@gmail.com>
2025-08-08 21:28:44 +0100

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,10 @@ import RegisterScreen from "./pages/Register";
 import LoginScreen from "./pages/Login";
 import CustomerLayout from "./pages/customer/CustomerLayout";
 import CustomerHome from "./pages/customer/Home";
-import CustomerFeed from "./pages/customer/Feed"
-import CustomerLive from "./pages/customer/Live"
+import CustomerFeed from "./pages/customer/Feed";
+import CustomerLive from "./pages/customer/Live";
 import CustomerProfile from "./pages/customer/Profile";
-import CustomerPunters from "./pages/customer/Punters"
+import CustomerPunters from "./pages/customer/Punters";
 import CustomerSearch from "./pages/customer/Search";
 import PunterLayout from "./pages/punter/PunterLayout";
 import PunterHome from "./pages/punter/Home";
@@ -26,46 +26,49 @@ import PunterCreate from "./pages/punter/Create";
 
 const App = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/welcome" element={<WelcomeScreen />} />
-        <Route path="/register" element={<RegisterScreen />} />
-        <Route path="/login" element={<LoginScreen />} />
+    <div>
+      <h1>The Punter Hub</h1>
+    </div>
+    // <Router>
+    //   <Routes>
+    //     <Route path="/" element={<SplashScreen />} />
+    //     <Route path="/welcome" element={<WelcomeScreen />} />
+    //     <Route path="/register" element={<RegisterScreen />} />
+    //     <Route path="/login" element={<LoginScreen />} />
 
-        <Route
-          path="/customer/*"
-          element={
-            // <ProtectedRoute>
-            <CustomerLayout />
-            // {/* </ProtectedRoute> */}
-          }
-        >
-          <Route index element={<CustomerHome />} />
-          <Route path="home" element={<CustomerHome />} />
-          <Route path="feed" element={<CustomerFeed />} />
-          <Route path="live" element={<CustomerLive />} />
-          <Route path="profile" element={<CustomerProfile />} />
-          <Route path="punters" element={<CustomerPunters />} />
-          <Route path="search" element={<CustomerSearch />} />
-        </Route>
-        <Route
-          path="/punter/*"
-          element={
-            // <ProtectedRoute>
-            <PunterLayout />
-            // {/* </ProtectedRoute> */}
-          }
-        >
-          <Route index element={<PunterHome />} />
-          <Route path="home" element={<PunterHome />} />
-          <Route path="history" element={<PunterHistory />} />
-          <Route path="profile" element={<PunterProfile />} />
-          <Route path="live" element={<PunterLive />} />
-          <Route path="create" element={<PunterCreate />} />
-        </Route>
-      </Routes>
-    </Router>
+    //     <Route
+    //       path="/customer/*"
+    //       element={
+    //         // <ProtectedRoute>
+    //         <CustomerLayout />
+    //         // {/* </ProtectedRoute> */}
+    //       }
+    //     >
+    //       <Route index element={<CustomerHome />} />
+    //       <Route path="home" element={<CustomerHome />} />
+    //       <Route path="feed" element={<CustomerFeed />} />
+    //       <Route path="live" element={<CustomerLive />} />
+    //       <Route path="profile" element={<CustomerProfile />} />
+    //       <Route path="punters" element={<CustomerPunters />} />
+    //       <Route path="search" element={<CustomerSearch />} />
+    //     </Route>
+    //     <Route
+    //       path="/punter/*"
+    //       element={
+    //         // <ProtectedRoute>
+    //         <PunterLayout />
+    //         // {/* </ProtectedRoute> */}
+    //       }
+    //     >
+    //       <Route index element={<PunterHome />} />
+    //       <Route path="home" element={<PunterHome />} />
+    //       <Route path="history" element={<PunterHistory />} />
+    //       <Route path="profile" element={<PunterProfile />} />
+    //       <Route path="live" element={<PunterLive />} />
+    //       <Route path="create" element={<PunterCreate />} />
+    //     </Route>
+    //   </Routes>
+    // </Router>
   );
 };
 
