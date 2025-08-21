aa6a19afcdc0aa32f05d5e6dfdf36937e0861fd4
ikosong14 <alexandervirtuous14@gmail.com>
2025-08-21 08:37:59 +0100

    modified:   src/components/Api.js

--- src/components/Api.js
+++ src/components/Api.js
@@ -1,4 +1,4 @@
-// const Api = "https://punterhub-backend.onrender.com";
-const Api = "http://192.168.1.107:5000"
+const Api = "https://punterhub-backend.onrender.com";
+// const Api = "http://192.168.1.107:5000"
 
 export default Api;
